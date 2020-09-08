a47306e7715c2cddb65d19ad488405ee0fe6a646
nendanurramadanti <59640312+nendanurramadanti@users.noreply.github.com>
2020-09-08 14:58:24 +0700

    Add files via upload

---
+++ Ulangan1.java
@@ -0,0 +1,21 @@
+package ulangan1;
+
+
+public class Ulangan1 {
+
+    public static void main(String[] args) {
+        String chara = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-#";
+        char ch1 = chara.charAt(13);
+        char ch2 = chara.charAt(4);
+        char ch3 = chara.charAt(13);
+        char ch4 = chara.charAt(3);
+        char ch5 = chara.charAt(0);
+        char ch6 = chara.charAt(13);
+        char ch7 = chara.charAt(20);
+        char ch8 = chara.charAt(17);
+        
+        System.out.println("" + ch1 + ch2 + ch3 + ch4 + ch5 + ch6 + ch7 + ch8);
+        
+    }
+    
+}
